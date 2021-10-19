9949e32f728705818b9753e1f8749060f59e954b
Bishoy <bishoy.samwuel@gmail.com>
2021-10-19 18:09:02 +0200

    Add structure of Calculator buttons

---
+++ src/components/Calculator.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Calculator = () => (
+  <div className="container">
+    <button type="button" className="col-4" id="result"> 0 </button>
+    <button type="button" className="bg-grey"> AC </button>
+    <button type="button" className="bg-grey"> +/- </button>
+    <button type="button" className="bg-grey"> % </button>
+    <button type="button" className="bg-orange"> + </button>
+    <button type="button" className="bg-grey"> 7 </button>
+    <button type="button" className="bg-grey"> 8 </button>
+    <button type="button" className="bg-grey"> 9 </button>
+    <button type="button" className="bg-orange"> x </button>
+    <button type="button" className="bg-grey"> 4 </button>
+    <button type="button" className="bg-grey"> 5 </button>
+    <button type="button" className="bg-grey"> 6 </button>
+    <button type="button" className="bg-orange"> - </button>
+    <button type="button" className="bg-grey"> 1 </button>
+    <button type="button" className="bg-grey"> 2 </button>
+    <button type="button" className="bg-grey"> 3 </button>
+    <button type="button" className="bg-orange"> + </button>
+    <button type="button" className="col-2 bg-grey"> 0 </button>
+    <button type="button" className="bg-grey"> . </button>
+    <button type="button" className="bg-orange"> = </button>
+  </div>
+);
+
+export default Calculator;
