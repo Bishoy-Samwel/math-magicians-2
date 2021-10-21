a9e2377f2d86889c8ce71e182a0ac96590150ef5
Bishoy <bishoy.samwuel@gmail.com>
2021-10-21 13:12:06 +0200

    - Convert container to function component - Add UseState hook for dataObj - Add handleClick function to update DataObj value when key pressed - Add logic to get the result value - Calling Caclulator component and passing result and handleClick

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Calculator from './Calculator';
+import calculate from '../Logic/Calculate';
 
-export default class Container extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export default function Container() {
+  const [dataObj, setDataObj] = useState({
+    total: null, next: null, operation: null,
+  });
 
-  render() {
-    return (
-      <Calculator />
-    );
-  }
+  const handleClick = event => {
+    const keyPress = event.target.getAttribute('btn_name');
+    setDataObj({ ...dataObj, ...calculate(dataObj, keyPress) });
+  };
+
+  const { total, next, operation } = dataObj;
+  const result = next || operation || total || '0';
+  return (
+    <Calculator
+      result={result}
+      handleClick={handleClick}
+    />
+  );
 }
